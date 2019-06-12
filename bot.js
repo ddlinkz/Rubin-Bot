400a666b0b9385cfd3135c5152da12c1daf55907
Austin Brown <ddlinkab@gmail.com>
2019-06-12 18:20:41 -0400

    Add MongoDB insert and connection

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 var Twit = require('twit');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 var Bot = new Twit({
     consumer_key: process.env.BOT_CONSUMER_KEY,
@@ -7,14 +9,37 @@ var Bot = new Twit({
     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
+var dbInfo = process.env.USERINFO;
+
+const uri = "mongodb+srv://"+dbInfo+"@cluster0-zd3dc.azure.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
 Bot.get('statuses/user_timeline', { screen_name: 'RickRubin'}, function(err, data, response) {
     console.log(data[0]);
 })
+
+client.connect(err => {
+    const collection = client.db("rubin-bot").collection("tweets");
+    collection.insertMany([
+        {a : 1}, {a : 2}, {a : 3}
+      ], function(err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+      });
+    // perform actions on the collection object
+    client.close();
+});
 
 // TODO:
 
 // Local testing - heroku local worker (done)
 // - also added .env and .gitignore files for local testing
 // Access media (find out which files to keep)
+// - created_at
+// - id:
+// - entities:
+// - extended_entities
 // Set up DB to download to
 // set up front end to previous tweets
