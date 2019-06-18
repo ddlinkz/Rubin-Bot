b1339b64ef8cc83df0338cddf2047b306788e40c
Austin Brown <ddlinkab@gmail.com>
2019-06-18 17:36:37 -0400

    Fix addition of existing Tweet

--- bot.js
+++ bot.js
@@ -27,28 +27,34 @@ Bot.get('statuses/user_timeline', { screen_name: 'RickRubin'}, function(err, dat
     var cloud_secure_url;
     var created_at = data[0].created_at;
     var user_count = data[0].user.followers_count;
-    cloudinary.uploader.upload(img_url,
-        { folder: "images/" },
-        function(error, result){
-            console.log(result, error);
-            cloud_url = result.url;
-            cloud_secure_url = result.secure_url;
-            client.connect(err => {
-                const collection = client.db("rubin-bot").collection("tweets");
-                var tweet = {
-                    img: cloud_url,
-                    secure_img: cloud_secure_url,
-                    date: created_at,
-                    user_count: user_count
+    var tweet_id = data[0].id;
+    client.connect(err => {
+        var Tweets = client.db("rubin-bot").collection("tweets").find({tweet_id: tweet_id}).toArray();
+        if(Tweets > 0){
+            cloudinary.uploader.upload(img_url,
+                { folder: "images/" },
+                function(error, result){
+                    console.log(result, error);
+                    cloud_url = result.url;
+                    cloud_secure_url = result.secure_url;
+                    var tweet = {
+                        tweet_id: tweet_id,
+                        img: cloud_url,
+                        secure_img: cloud_secure_url,
+                        date: created_at,
+                        user_count: user_count
+                    }
+                    Tweets.insertOne(tweet, function(err, res) {
+                        if(err) throw err;
+                        console.log("1 document inserted");
+                    })
                 }
-                collection.insertOne(tweet, function(err, res) {
-                    if(err) throw err;
-                    console.log("1 document inserted");
-                })
-                client.close();
-            });
+            )
+        } else {
+            console.log("Tweet exists already!");
         }
-    )
+        client.close();
+    });
 });
 
 // TODO:
