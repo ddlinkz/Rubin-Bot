3a73675363bb8855d54b9c5025cf716f7f44178b
Austin Brown <ddlinkab@gmail.com>
2021-08-26 23:49:03 -0700

    Uncomment build path

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Serve static files from React
-//app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
