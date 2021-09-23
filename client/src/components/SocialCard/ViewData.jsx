b295babcd73a8de97412dbb3431ad00c4a4d6645
Austin Brown <ddlinkab@gmail.com>
2021-09-23 12:22:25 -0700

    Commit ViewData move

--- client/src/components/SocialCard/ViewData.jsx
+++ client/src/components/SocialCard/ViewData.jsx
@@ -5,10 +5,20 @@ import styled from 'styled-components';
 const ViewDataContainer = styled.div`
 	width: 200px;
 	height: 80px;
+	float: left;
 	font-family: inherit;
-	display: inline-block;
 	border: 1px black solid;
+	border-radius: 25px;
 	font-style: italic;
+
+	@media (max-width: 900px) {
+		width: 170px;
+	}
+
+	@media (max-width: 450px) {
+		flex-grow: 1;
+		height: 8%;
+	}
 `
 
 const ViewData = ({children}) => {
