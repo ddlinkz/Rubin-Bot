11900be97c18c6966c8d60d3130a78d4b660926e
Austin Brown <ddlinkab@gmail.com>
2021-09-24 13:51:00 -0700

    Add modified App index

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,30 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { FrontPage, TweetPage, SearchPage, AboutPage } from '../pages';
-import { Header, Footer } from '../components';
+import { ApiWrapper } from '../components';
 
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
-  position: relative;
-  min-height: 100vh;
-  padding-bottom: 7rem;
+    position: relative;
+    min-height: 100vh;
+    padding-bottom: 7rem;
 `
 
-function App() {
-  return (
-    <AppContainer>
-      <Router>
-        <Header />
-        <Route path="/" exact component={FrontPage} />
-        <Route path="/tweets/:tweet_id" component={TweetPage} />
-        <Route path="/search" component={SearchPage} />
-        <Route path="/about" component={AboutPage} />
-        <Footer />
-      </Router>
-    </AppContainer>
-  );
+class App extends React.Component {
+    render () {
+        return (
+            <AppContainer>
+                <ApiWrapper />
+            </AppContainer>
+        )
+    }
 }
 
 export default App;
